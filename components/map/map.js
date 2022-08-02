98c27e6568bb0ef3a23de4dcdf2582413119460c
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-08-02 21:26:39 +0530

    International Space Station Live Location works

--- components/map/map.js
+++ components/map/map.js
@@ -8,13 +8,16 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 
 export default function Map({lat,long,time,e}){
-let z =3;
+// let z =3;
+let z= 10.2;
+const [zoom,setZoom] = useState(3);
+
   const [viewport,setViewport] = useState({
   // style: 'mapbox://styles/mapbox/streets-v10',
   projection: 'globe',
   latitude: lat ,
   longitude: long,
-  zoom: z,
+  zoom: zoom,
   center: [long, lat],
   width: "600px",
   height: "500px"
@@ -31,14 +34,14 @@ useEffect((e)=>{
     projection: 'globe',
     latitude: lat ,
     longitude: long,
-    zoom: z,
+    zoom: zoom,
     center: [long, lat],
     width: "600px",
     height: "500px"
   });
 },[e]);
 
-
+// console.log(e);
 
 
   return(
@@ -56,7 +59,9 @@ useEffect((e)=>{
         >
           
           <Marker longitude={long} latitude={lat} anchor="bottom" >
-            <img className='w-8 h-8' src="/img/mapbox-icon.png" />
+            {/* <img className='w-8 h-8' src="/img/mapbox-icon.png" /> */}
+            {/* <svg className='w-4 animate-pin h-4 rotate-180 shadow-2xl drop-shadow-2xl   ' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 22h-24l12-20z"/></svg> */}
+            <img className='w-6 animate-pin h-6 rotate-45 shadow-2xl drop-shadow-2xl' src="/international-space-station-svgrepo-com.svg"></img>
           </Marker>
         <GeolocateControl
           positionOptions={{ enableHighAccuracy: true }}
@@ -67,12 +72,12 @@ useEffect((e)=>{
         </ReactMapGL>
         
         
-        <div className='text-base pt-5 flex flex-row items-start justify-start space-x-2'>
+        <div className='text-sm pt-5 flex flex-row items-start justify-start space-x-2'>
           <div>Incoming data: </div>
           {
             e &&
-            <div className='bg-gray-100 rounded-full p-1'>
-              <svg className='fill-green-600' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/></svg>
+            <div className='bg-gray-10 rounded-full p-1'>
+              <svg className='fill-gre-600 w-5 h-5 animate' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/></svg>
             </div>
           }
           {
@@ -81,11 +86,18 @@ useEffect((e)=>{
               <svg className='rotate-180 fill-red-600' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3.698 15.354c-.405-.031-.367-.406.016-.477.634-.117.913-.457.913-.771 0-.265-.198-.511-.549-.591-.418-.095-.332-.379.016-.406.566-.045.844-.382.844-.705 0-.282-.212-.554-.63-.61-.429-.057-.289-.367.016-.461.261-.08.677-.25.677-.755 0-.336-.25-.781-1.136-.745-.614.025-1.833-.099-2.489-.442.452-1.829.343-4.391-.845-4.391-.797 0-.948.903-1.188 1.734-.859 2.985-2.577 3.532-4.343 3.802v4.964c3.344 0 4.25 1.5 6.752 1.5 1.6 0 2.426-.867 2.426-1.333 0-.167-.136-.286-.48-.313z"/></svg>
             </div>
           }
-          {e && 
-          <div>
-           
-          </div>}
+          
         </div>
+        <div className='flex flex-row items-start justify-start space-x-3'><br></br>
+           { zoom==10.2 && <button onClick={()=>{
+            setZoom(3);
+           }} className='bg-gray-900 scale-7 rounded p-2  text-gray-100 font-sans text-sm tracking-wide' >Globe View</button>
+           }
+           
+           {zoom==3 && <button onClick={()=>{
+            setZoom(10.2);
+           }} className='bg-gray-900 scale-7 rounded p-2  text-gray-100 font-sans text-sm tracking-wide' >Street View</button>
+          }</div>
     </div>
   );
 }
--- pages/tools/live-location/index.js
+++ pages/tools/live-location/index.js
@@ -45,23 +45,26 @@ async function getData(){
   return(
     <div className="h-screen flex flex-col  items-center space-y-2 justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
        <Head>
-        <title>ISS Live Location</title>
+        <title>International Space Station Live Location</title>
         <link rel="icon" href="/favicon.ico" />
 
 
       </Head>
-      <div className="flex flex-row items-center justify-center space-x-3">
-      <div>Live Location</div>
+      <div className="flex flex-col items-center justify-center space-y-1">
       {/* <Load></Load> */}
-      <div className="group relative  transition-all duration-1000 ease-in-out ">
+      <div className='w-full flex flex-row items-center justify-center space-x-3'>
+
+      <div className='text-2xl tracking-widest'>International Space Station Live Location</div>
+      <div className="flex flex-row space-x-3 group relative  transition-all duration-1000 ease-in-out ">
         <svg className="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm1-6h-2v-8h2v8zm-1-12.25c.69 0 1.25.56 1.25 1.25s-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25.56-1.25 1.25-1.25z"/></svg>
-        <div className="hidden opacity-0 group-hover:opacity-80 group-hover:inline-block group-hover:absolute bg-red- translate-x-5 group-hover:translate-x-0 transition-all duration-1000 ease-in-out -top-14  left-10 w-[300px]">
+        <div className="bg-gray-100/95 rounded p-3 tracking-widest hidden opacity-0 group-hover:opacity-80 group-hover:inline-block group-hover:absolute bg-red- translate-x-5 group-hover:translate-x-0 transition-all duration-1000 ease-in-out -top-14  left-10 w-[300px]">
           The International Space Station is moving at close to 28,000 km/h
           <br></br>
         </div>
       </div>
+      </div>
+      <Link href="http://open-notify.org/Open-Notify-API/ISS-Location-Now/"><a className='font-semibold underline'>API</a></Link>
       <br></br>
-          <Link href="http://open-notify.org/Open-Notify-API/ISS-Location-Now/"><a className='font-semibold underline'>API</a></Link>
       </div>
       <div className="flex  flex-col items-start justify-start lg:flex-row lg:items-center lg:justify-center space-x-3">
         { e && <LiveLocation e={e} lat={Lat} long={Long} time={time}></LiveLocation>}
@@ -97,28 +100,28 @@ function LiveAPIdata({lat,long,time,e}){
      }
     {
       e && 
-      <div className='w-[250px] h[250px]'><Date  lat={lat} long={long} time={time} e={e} ></Date></div>
+      <div className='w-[250px] h-[500px] flex flex-col items-center justify-center'><Date  lat={lat} long={long} time={time} e={e} ></Date></div>
     }
     </div>
   );
 }
 function Date({lat,long,time,e}){
   return(
-    <div className='w-full'>
+    <div className='w-full flex flex-col items-center justify-around space-y-10'>
       {/* <div className='font-sans text-xl mb-5'>Location: ISS</div> */}
-      <div className='w-full flex flex-row items-start justify-start'>
-        <div className='w-1/3 font-bold'>Latitude</div>
-        <div className='w-2/3'>{lat}</div>
+      <div className='w-full flex flex-col items-center justify-center tracking-widest'>
+        <div className='w- font-bo  text-2xl '>Latitude</div>
+        <div className='w text-base -'>{lat}</div>
       </div>
 
-      <div className='w-full flex flex-row items-start justify-start'>
-        <div className='w-1/3 font-bold'>Longitude</div>
-        <div className='w-2/3'>{long}</div>
+      <div className='w-full flex flex-col items-center justify-center tracking-widest'>
+        <div className='-1/3 font-old  text-2xl '>Longitude</div>
+        <div className='-2/3 text-base '>{long}</div>
       </div >
 
-      <div className='w-full flex flex-row items-start justify-start'>
-        <div className='w-1/3 font-bold' >Time</div>
-        <div className='w-2/3' >{time}</div>
+      <div className='w-full flex flex-col items-center justify-center tracking-widest'>
+        <div className='-1/3 fontbold  text-2xl ' >Time</div>
+        <div className='-2/3' text-base  >{time}</div>
       </div>
 
     </div>
